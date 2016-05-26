fcb4539fefb7b6fb23b57803a826a31906a13c53
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2016-05-26 10:53:31 +0200

    minor issue with filters

--- src/Ecommerce/BackendBundle/Form/Type/OrderSearchType.php
+++ src/Ecommerce/BackendBundle/Form/Type/OrderSearchType.php
@@ -13,7 +13,6 @@ class OrderSearchType extends AbstractType
     {
         $builder->add('from', 'date', array('widget' => 'single_text', 'required' => false, 'format' => 'dd-MM-yyyy'))
                 ->add('to', 'date', array('widget' => 'single_text', 'required' => false, 'format' => 'dd-MM-yyyy'))
-                ->add('payment', 'checkbox', array('required' => false))
                 ->add('status', 'choice', array(
                 'choices' => array(Order::STATUS_READY => Order::STATUS_READY, Order::STATUS_IN_PROCESS => Order::STATUS_IN_PROCESS, Order::STATUS_OUT_OF_STOCK => Order::STATUS_OUT_OF_STOCK,
                                     Order::STATUS_READY_TO_TAKE => Order::STATUS_READY_TO_TAKE, Order::STATUS_SEND => Order::STATUS_SEND, Order::STATUS_CANCELED => Order::STATUS_CANCELED),
